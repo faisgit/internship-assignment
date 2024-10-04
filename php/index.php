32e9bd313ba37d213d7a62ce46e0e14236f8fa0e
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2024-10-04 17:21:59 +0530

    solving php assignment

---
+++ php/index.php
@@ -0,0 +1,117 @@
+<?php
+// Basic
+
+// <!-- WAP to read 2 numbers and print addition of those two numbers  -->
+
+    // $numOne = readline('Enter Number One: ');
+    // $numTwo =  readline("Enter Number Two: ");
+    // $add =  $numOne + $numTwo;
+    // echo "$numOne + $numTwo = $add"
+
+
+    // WAP to read 2 numbers and print substraction
+
+    // $numOne = readline("Enter First Number: ");
+    // $numTwo = readline("Enter Second Number: ");
+    // $sub = $numOne -  $numTwo;
+    // echo "$numOne - $numTwo =  $sub"
+
+     // WAP to read 2 numbers and print multiplication
+
+    // $numOne = readline("Enter First Number: ");
+    // $numTwo = readline("Enter Second Number: ");
+    // $product = $numOne * $numTwo;
+    // echo "$numOne * $numTwo =  $product"
+
+
+    // WAP to read 2 numbers and print division
+
+    // $numOne = readline("Enter First Number: ");
+    // $numTwo = readline("Enter Second Number: ");
+    // $division = $numOne * $numTwo;
+    // echo "$numOne * $numTwo =  $division"
+
+
+    // WAP to read 2 numbers and print modulus (Remainder)
+
+    // $numOne = readline("Enter First Number: ");
+    // $numTwo = readline("Enter Second Number: ");
+    // $modulus = $numOne * $numTwo;
+    // echo "$numOne * $numTwo =  $modulus"
+
+    // WAP to read radius and print area and circumference of circle
+
+    // $radius = readline("Enter the radius of Circle: ");
+    // $circumference =  2 * 3.14 *  $radius;
+    // echo $circumference; 
+
+    // WAP to read length and breadth and print area and perimeter of rectangle
+
+    // $l = readline("Enter the length of rectangle: ");
+    // $b =  readline("Enter the breadth of rectangle: ");
+
+    // $perimeter = 2 *($l + $b);
+
+    // echo "perimeter of rectangle is $perimeter \n";
+    // $area = $l * $b;
+    // echo "area of rectangle is $area";
+
+
+    // Conditional Statement (if else)
+
+    // 1. WAP to read a number and check if its positive or negative
+
+    // if ($num > 0) {
+    //     # code...
+    //     echo "The given number is positive";
+    // }
+    // else{
+    //     echo "The given number is negetive";
+    // }
+
+    // 2. WAP to read a number and check it is even or odd
+    
+        // $num = readline("Enter number: ");
+        // if ($num % 2 == 0) {
+        //     # code...
+        //     echo "Even";
+        // }
+        // else{
+        //     echo 'Odd';
+        // }
+
+        // WAP to read 2 numbers and find greatest among them
+        // $numOne = readline("Enter First Number: ");
+        // $numTwo = readline("Enter Second Number: ");
+
+        // if ($numOne > $numTwo) {
+        //     # code...
+        //     echo "Num One is greater";
+        // }
+        // else{
+        //     echo "Num Two is greater";
+        // }
+
+    // WAP to read 3 numbers and find greatest among them
+    // $a = 56;
+    // $b = 67;
+    // $c = 2;
+    // if ($a > $b  && $a > $c) {
+    //     # code...
+    //     echo "a is greater";
+    // }
+    // else if($b > $c && $b > $a){
+    //     echo "b is greater";
+    // }
+    // else{
+    //     echo "c is greater";
+    // }
+
+
+//     5. WAP to read marks of 5 subjects and check the student is pass or failed
+
+// 1. add validation for marks less than o
+
+// 2. add validation for marks greater than 100
+
+?>
